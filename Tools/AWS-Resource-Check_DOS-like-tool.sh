1667143ab60cb6786bb5b66bee9f12821802cda8
vzlatkov <vzlatkov@xi-group.com>
2021-08-06 14:21:07 +0300

    v1.0.3

--- Tools/AWS-Resource-Check_DOS-like-tool.sh
+++ Tools/AWS-Resource-Check_DOS-like-tool.sh
@@ -4,6 +4,10 @@
 ###This is version 1.0.1
 ###In this revision the basic layout of the script will be created such as the UI and some of the options. Also, there are some pre-requsites that would be needed, so the script would make dependency checks as well.
 ###This is version 1.0.2 
+###This is version 1.0.3
+###In this revision, the options are hardcoded. It acctually do not need to be that complicated, but the purpose of the script is to work and to help me practice working with the "dialog" utility. It was going to be much more easier without using it.
+###You should choose the tags for the services you want to check and it redirects it to a file called "TNT_`name_of_tag`" in your $PWD, so you can later cat TNT* and get all of the services' names that should be redeployed with the new tags acording to TNT.
+###I should add two new dialog boxes, so you can check all of the services at once... (or you know... I can just execute the aws command and not bother making useless scripts)
 
 cr=`echo $'\n'`
 version="version 1.0.1"
@@ -31,74 +35,63 @@ CHOICE_HEIGHT=4
 BACKTITLE="AWS Util $version"
 TITLE="AWS Util $version"
 MENU="Choose one of the following options:"
-MENU2="Choose your AWS username"
+MENU2="Choose the desired AWS TAG"
 
-OPTIONS=(1 "Region"
-         2 "Tag"
-         3 "Service")
-OPTIONS2=(1 "Region"
-         2 "Tag"
-         3 "Service")
+OPTIONS=(1 "service"
+         2 "Service"
+         3 "Type"
+         4 "type")
 
 CHOICE=$(dialog --clear \
-		--backtitle "$BACKTITLE" \
+        --backtitle "$BACKTITLE" \
                 --title "$TITLE" \
                 --menu "$MENU" \
                 $HEIGHT $WIDTH $CHOICE_HEIGHT \
                 "${OPTIONS[@]}" \
                 2>&1 >/dev/tty)
+    clear
+OPTIONS2=(1 "service"
+         2 "Service"
+         3 "Type"
+         4 "type")
+
 CHOICE2=$(dialog --clear \
-                --backtitle "$BACKTITLE" \
+        --backtitle "$BACKTITLE" \
                 --title "$TITLE" \
                 --menu "$MENU" \
                 $HEIGHT $WIDTH $CHOICE_HEIGHT \
                 "${OPTIONS2[@]}" \
                 2>&1 >/dev/tty)
-CHOICE3=$(dialog --clear \
-		# --title "$TITLE" \
-		 #--menu "$MENU2" \
-		 #--textbox \
-		 $HEIGHT $WIDTH $CHOICE_HEIGHT \
-		 2>&1 >/dev/tty)
-	clear
+    clear
+
 case $CHOICE in
         1)
-           read -p "Please select region $cr" region
+           aws ec2 describe-tags --filters "Name=tag:service,Values=*" --output text | awk '{print $5}' | sort | uniq #1>$PWD/TNT_service
             ;;
         2)
-            echo "You chose Option 2"
+           aws ec2 describe-tags --filters "Name=tag:Service,Values=*" --output text | awk '{print $5}' | sort | uniq 1>$PWD/TNT_Service
             ;;
         3)
-            echo "You chose Option 3"
+            aws ec2 describe-tags --filters "Name=tag:Type,Values=*" --output text | awk '{print $5}' | sort | uniq 1>$PWD/TNT_Type 
+            ;;
+        4)  
+            aws ec2 describe-tags --filters "Name=tag:type,Values=*" --output text | awk '{print $5}' | sort | uniq 1>$PWD/TNT_type 
             ;;
 esac
+
 case $CHOICE2 in
         1)
-           read -p "Please select region $cr" region
-
+           aws ec2 describe-tags --filters "Name=tag:service,Values=*" --output text | awk '{print $5}' | sort | uniq 1>$PWD/TNT_service 
             ;;
         2)
-		read -p "Please select a TAG (ex.:'tag=name_of_the_tag') $cr" tag
+           aws ec2 describe-tags --filters "Name=tag:Service,Values=*" --output text | awk '{print $5}' | sort | uniq 1>$PWD/TNT_Service 
             ;;
         3)
-            echo "You chose Option 3"
+            aws ec2 describe-tags --filters "Name=tag:Type,Values=*" --output text | awk '{print $5}' | sort | uniq 1>$PWD/TNT_Type 
+            ;;
+        4)  
+            aws ec2 describe-tags --filters "Name=tag:type,Values=*" --output text | awk '{print $5}' | sort | uniq 1>$PWD/TNT_type 
             ;;
 esac
 
-#case $CHOICE3 in
-  #      1)
- #          read -p "Please select region $cr" region
-#
-       #     ;;
-      #  2)
-     #           read -p "Please select a TAG (ex.:'tag=name_of_the_tag') $cr" tag
-    #        ;;
-   #     3)
-  #          echo "You chose Option 3"
- #           ;;
-#esac
 
-aws ec2 describe-tags --filters "Name=tag:Monitoring,Values=$tag" "Name=tag:service,Values=*" --output text | awk '{print $5}' |sort |uniq
-#aws s3 describe-tags --filters "Name=tag:Zoo,Values=$tag" "Name=tag:Monitoring,Values=development" --output text
-aws ec2 describe-tags --filters "Name=tag:Zoo,Values=sofiazoo" --output text | awk '{print $5}' | sort | uniq
-aws ec2 describe-tags --filters "Name=tag:service,Values=*" --output text | awk '{print $5}' | sort | uniq
